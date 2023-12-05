09d5d5a0e7700cf07c0ca469315f53251a7f08ef
Ranonx <108208227+Ranonx@users.noreply.github.com>
2023-12-05 23:38:11 +0800

    middleware serve static

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const { init: initDB, Counter } = require("./db");
+/** @format */
 
-const logger = morgan("tiny");
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const { init: initDB, Counter } = require('./db');
+
+const logger = morgan('tiny');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -12,17 +14,20 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 // 首页
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get('/', async (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // 更新计数
-app.post("/api/count", async (req, res) => {
+app.post('/api/count', async (req, res) => {
   const { action } = req.body;
-  if (action === "inc") {
+  if (action === 'inc') {
     await Counter.create();
-  } else if (action === "clear") {
+  } else if (action === 'clear') {
     await Counter.destroy({
       truncate: true,
     });
@@ -34,7 +39,7 @@ app.post("/api/count", async (req, res) => {
 });
 
 // 获取计数
-app.get("/api/count", async (req, res) => {
+app.get('/api/count', async (req, res) => {
   const result = await Counter.count();
   res.send({
     code: 0,
@@ -43,9 +48,9 @@ app.get("/api/count", async (req, res) => {
 });
 
 // 小程序调用，获取微信 Open ID
-app.get("/api/wx_openid", async (req, res) => {
-  if (req.headers["x-wx-source"]) {
-    res.send(req.headers["x-wx-openid"]);
+app.get('/api/wx_openid', async (req, res) => {
+  if (req.headers['x-wx-source']) {
+    res.send(req.headers['x-wx-openid']);
   }
 });
 
@@ -54,7 +59,7 @@ const port = process.env.PORT || 80;
 async function bootstrap() {
   await initDB();
   app.listen(port, () => {
-    console.log("启动成功", port);
+    console.log('启动成功', port);
   });
 }
 
