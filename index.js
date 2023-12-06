033308ba24014906aac10416a974bae0f89cdba4
Ranonx <108208227+Ranonx@users.noreply.github.com>
2023-12-06 18:06:53 +0800

    removed unneccsary details

--- index.js
+++ index.js
@@ -3,16 +3,12 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
-const { init: initDB, Counter } = require('./db');
-
-const logger = morgan('tiny');
+// const { init: initDB, Counter } = require('./db');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-app.use(logger);
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -41,38 +37,6 @@ app.get('/poster5', (req, res) => {
 // 首页
 app.get('/', async (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-// 更新计数
-app.post('/api/count', async (req, res) => {
-  const { action } = req.body;
-  if (action === 'inc') {
-    await Counter.create();
-  } else if (action === 'clear') {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
-});
-
-// 获取计数
-app.get('/api/count', async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
-});
-
-// 小程序调用，获取微信 Open ID
-app.get('/api/wx_openid', async (req, res) => {
-  if (req.headers['x-wx-source']) {
-    res.send(req.headers['x-wx-openid']);
-  }
 });
 
 const port = process.env.PORT || 80;
