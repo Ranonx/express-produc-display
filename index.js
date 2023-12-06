fb0145e7ea866e5882236fb22a3f0cc21031b664
Ranonx <108208227+Ranonx@users.noreply.github.com>
2023-12-06 18:11:03 +0800

    put back the details plus css

--- index.js
+++ index.js
@@ -3,12 +3,16 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
-// const { init: initDB, Counter } = require('./db');
+const morgan = require('morgan');
+const { init: initDB, Counter } = require('./db');
+
+const logger = morgan('tiny');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use(logger);
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -37,6 +41,22 @@ app.get('/poster5', (req, res) => {
 // 首页
 app.get('/', async (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// 获取计数
+app.get('/api/count', async (req, res) => {
+  const result = await Counter.count();
+  res.send({
+    code: 0,
+    data: result,
+  });
+});
+
+// 小程序调用，获取微信 Open ID
+app.get('/api/wx_openid', async (req, res) => {
+  if (req.headers['x-wx-source']) {
+    res.send(req.headers['x-wx-openid']);
+  }
 });
 
 const port = process.env.PORT || 80;
